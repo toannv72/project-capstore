bedc71930bd7437970bbf8484395db6b74dc2dfc
nguyen van toan <toansola17122000@gmail.com>
2024-05-06 03:30:02 +0700

    fix comHeader

--- src/Components/ComHeader/ComHeader.jsx
+++ src/Components/ComHeader/ComHeader.jsx
@@ -334,13 +334,13 @@ export default function ComHeader({ children }) {
           </Disclosure>
         </Affix>
 
-        <header className="bg-white shadow">
+        {/* <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">
               Dashboard
             </h1>
           </div>
-        </header>
+        </header> */}
         <main>
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             {children}
