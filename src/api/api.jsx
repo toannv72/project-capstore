ac9284ae81a09b0ff44149e3581d1e397a5619d5
H <110007694+hohongminh@users.noreply.github.com>
2024-08-30 19:23:40 +0700

    up

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: process.env.REACT_APP_BASE_URLS,
   // baseURL: "https://nursinghome.runasp.net/api/",
-  baseURL: "https://homenursingcatone.xyz/api/",
+  baseURL: "https://nursinghome.runasp.net/api/",
   withCredentials: true,
 });
 
